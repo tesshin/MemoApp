6cc8fec5e91326be962bcbebc575046263989458
tesshin <0301ktesshin@gmail.com>
2020-05-31 19:55:34 +0900

    Create MemoListScreen

---
+++ src/screens/MemoListScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import MemoList from '../components/MemoList';
+import Circlebutton from '../elements/Circlebutton';
+
+class MemoListScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <MemoList />
+                <Circlebutton>+</Circlebutton>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        width: '100%',
+    },
+});
+
+export default MemoListScreen;
