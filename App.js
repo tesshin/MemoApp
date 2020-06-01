17a7353ebe893548585645f0b6228b5989e1c9ca
tesshin <0301ktesshin@gmail.com>
2020-06-01 22:07:00 +0900

    Create Login and Signup

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import MemoDetailScreen from './src/screens/MemoEditScreen';
+import MemoDetailScreen from './src/screens/SignupScreen';
 import Appbar from './src/components/Appbar';
 
 
